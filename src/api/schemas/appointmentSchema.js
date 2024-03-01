94b06c0b2f23181f8dc884dc949cad26afd27947
loulevin <patricia.balbier@gmail.com>
2024-03-01 16:57:51 +0100

    add somethings

--- src/api/schemas/appointmentSchema.js
+++ src/api/schemas/appointmentSchema.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+
 export const appointmentSchema = new mongoose.Schema(
   {
     date: { type: Date, required: true },
--- src/api/server.js
+++ src/api/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { appointmentRouter } from "./routers/appointmentRouter.js";
@@ -6,8 +7,10 @@ import { appointmentRouter } from "./routers/appointmentRouter.js";
 
 dotenv.config();
 
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 const port = 4510;
 
 const dbUrl = process.env.DB_URL;
